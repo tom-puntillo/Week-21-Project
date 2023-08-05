4ef2bec8c3775601aba0ad110e1dff29d12b33d5
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-05 00:54:18 -0400

    created main.tf variable.tf and terraform.tf (#1)

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}

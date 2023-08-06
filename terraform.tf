aea0bbe2f147fa3305ef29c17201304747a46ba6
tom-puntillo <tom.puntillo@gmail.com>
2023-08-06 23:55:16 +0000

    updated the comments

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 # Specify the S3 bucket as the backend for Terraform state storage
 terraform {
   backend "s3" {
-    bucket = "my-terraform-luit-week21-state-tjp" # Replace with your S3 bucket name
-    key    = "aws/terraform"                      # Replace with the desired path to the state file within the bucket
-    region = "us-east-1"                          # Replace with your desired AWS region for the S3 bucket
+    bucket = "my-terraform-luit-week21-state-tjp" # Define your S3 bucket name
+    key    = "aws/terraform"                      # Define the desired path to the state file within the bucket
+    region = "us-east-1"                          # Define your desired AWS region for the S3 bucket
   }
 
   # Define the required version of Terraform for this configuration
